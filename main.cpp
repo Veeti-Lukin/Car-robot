1fc4c116bf480f9821272abca08e4424265dfcb3
Veeti Lukin <veeti.lukin@tuni.fi>
2023-11-19 17:51:48 +0200

    Add server functionality

--- main.cpp
+++ main.cpp
@@ -3,8 +3,28 @@
 #include "BCM2711_GPIO_DRIVER/inc/BCM2711_GPIO_Driver/bcm2711_gpio_driver.h"
 
 int main() {
+    using http::TcpServer;
+
     BCM2711_GPIO_DRIVER::initialize();
+
+    TcpServer server("0.0.0.0", 2234);
+    server.bindHandler(http::types::RequestMethod::GET, "/", [](http::HttpRequest a, http::HttpResponse b){
+        std::string server_message = "";
+        //std::getline(std::cin, server_message);
+        return TcpServer::buildTestResponse(server_message);});
+
+    server.bindHandler(http::types::RequestMethod::GET, "/start?", [](http::HttpRequest a, http::HttpResponse b){
+        std::string html = "<!DOCTYPE html><html lang=\"en\"><body><h1> HANDLER IS WORKING!!! </h1></body></html>";
+
+        http::HttpResponse response(http::types::HttpProtocolVersion::HTTP1_1, http::types::ResponseStatusCode::OK);
+        response.setContentBodyFormat(http::types::ContentBodyFormat::text_html);
+        response.setContentBody(html);
+
+        return response;
+    });
+    server.startServing();
+
     BCM2711_GPIO_DRIVER::terminate();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
