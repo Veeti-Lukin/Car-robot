d3302362f6713fa7defa5cafd46c99fa31173de8
Veeti Lukin <veeti.lukin@tuni.fi>
2024-01-23 12:46:58 +0200

    Add Piezo controller library

--- libs/piezo_controller/inc/piezo_controller/PiezoController.h
+++ libs/piezo_controller/inc/piezo_controller/PiezoController.h
@@ -4,28 +4,49 @@
 
 #include <cstdint>
 #include <thread>
+#include <vector>
+#include <chrono>
+#include <mutex>
+
+#include "software_pwm/SoftwarePwmController.h"
+
+using namespace std::chrono_literals;
 
 class PiezoController {
 public:
-    enum class PlayPriority{
-        queue,
-        override,
+    struct Sound {
+        uint16_t frequency;
+        std::chrono::milliseconds duration_ms;
+    };
+
+    enum class PlayingProtocol {
+        async,
+        blocking
     };
 
     PiezoController(uint8_t pin_num);
     ~PiezoController();
 
-    void asyncPlaySound(uint16_t frequency, int32_t duration_ms, PlayPriority
-    play_priority_ = PlayPriority::override);
-    void stopPlayingAndFlushQueue();
+    bool playSound(Sound sound, PlayingProtocol playing_protocol =
+            PlayingProtocol::async);
+    void stopPlaying();
+
+    bool playSoundPattern(std::vector<Sound> sounds,
+                          PlayingProtocol playing_protocol = PlayingProtocol::async);
 
     void setVolume(uint8_t volume_percentage);
 
-    void flushQueue();
+    bool isPlaying() const;
 private:
     uint8_t volume_percentage_ = 50;
+    bool is_playing_ = false;
+
+    SoftwarePwmController soft_pwm_controller_;
 
     std::thread work_thread;
+    std::mutex mutex_;
+    // raising this may cause the playing sound duration to overshoot
+    std::chrono::milliseconds thread_sync_interval_ = 10ms;
 };
 
 
--- libs/piezo_controller/src/PiezoController.cpp
+++ libs/piezo_controller/src/PiezoController.cpp
@@ -1,29 +1,139 @@
 
-#include "sound_controller/PiezoController.h"
 #include "piezo_controller/PiezoController.h"
 
-
-PiezoController::PiezoController(uint8_t pin_num) {
+PiezoController::PiezoController(uint8_t pin_num):
+soft_pwm_controller_(pin_num, 0, 0) {
 
 }
 
 PiezoController::~PiezoController() {
-
+    stopPlaying();
+    if(work_thread.joinable()) {
+        work_thread.join();
+    }
 }
 
-void PiezoController::asyncPlaySound(uint16_t frequency, int32_t duration_ms,
-                                PiezoController::PlayPriority play_priority_) {
-    work_thread = std::thread();
+bool PiezoController::playSound(PiezoController::Sound sound,
+                                PiezoController::PlayingProtocol playing_protocol) {
+    // sound should not be played if there is already a sound playing
+    if(is_playing_) return false;
+
+    work_thread = std::thread([&]{
+        uint8_t volume_percentage;
+        {
+            std::lock_guard<std::mutex> lock_guard(mutex_);
+            volume_percentage = volume_percentage_;
+            is_playing_ = true;
+        }
+
+        // greatest resonance(aka most volume) is achieved at 50% duty cycle
+        // going under or lower than that achieves just lowers the volume
+        uint8_t duty_cycle = volume_percentage/2;
+
+        soft_pwm_controller_.configure(sound.frequency, duty_cycle);
+        soft_pwm_controller_.enable(true);
+
+        std::chrono::time_point<std::chrono::system_clock> timeout_time_point =
+                std::chrono::system_clock::now() + sound.duration_ms;
+
+        // this loop runs until the duration given to <sound> is completed
+        // or if sound playing is interrupted with stopPlaying().
+        // This loop also syncs the volume if it is modified while the
+        // sound is playing
+        while (std::chrono::system_clock::now() < timeout_time_point) {
+            std::this_thread::sleep_for(thread_sync_interval_);
+            {
+                std::lock_guard<std::mutex> lock_guard(mutex_);
+                if(!is_playing_) break;
+
+                if(volume_percentage_ != volume_percentage) {
+                    volume_percentage = volume_percentage_;
+                    duty_cycle = volume_percentage/2;
+                    soft_pwm_controller_.configure(sound.frequency, duty_cycle);
+                }
+            }
+        }
+        soft_pwm_controller_.enable(false);
+                });
+
+    if(playing_protocol == PlayingProtocol::blocking) {
+        work_thread.join();
+    }
+
+    return true;
 }
 
-void PiezoController::stopPlayingAndFlushQueue() {
+void PiezoController::stopPlaying() {
+    is_playing_ = false;
+}
 
+bool PiezoController::playSoundPattern(std::vector<Sound> sounds,
+                                       PiezoController::PlayingProtocol playing_protocol) {
+    // sound should not be played if there is already a sound playing
+    if(is_playing_) return false;
+
+    work_thread = std::thread([&]{
+        uint8_t volume_percentage;
+        {
+            std::lock_guard<std::mutex> lock_guard(mutex_);
+            volume_percentage = volume_percentage_;
+            is_playing_ = true;
+        }
+
+        // greatest resonance(aka most volume) is achieved at 50% duty cycle
+        // going under or lower than that achieves just lowers the volume
+        uint8_t duty_cycle = volume_percentage/2;
+
+        // iterate over all the sounds
+        bool should_stop = false;
+        for(Sound sound : sounds) {
+            if(should_stop) break;
+
+            soft_pwm_controller_.configure(sound.frequency, duty_cycle);
+            soft_pwm_controller_.enable(true);
+
+            std::chrono::time_point<std::chrono::system_clock> timeout_time_point =
+                                                                       std::chrono::system_clock::now() +
+                                                                       sound.duration_ms;
+
+            // this loop runs until the duration given to <sound> is completed
+            // or if sound playing is interrupted with stopPlaying().
+            // This loop also syncs the volume if it is modified while the
+            // sound is playing
+            while (std::chrono::system_clock::now() < timeout_time_point) {
+                std::this_thread::sleep_for(thread_sync_interval_);
+                {
+                    std::lock_guard<std::mutex> lock_guard(mutex_);
+                    if (!is_playing_) {
+                        should_stop = true;
+                        break;
+                    }
+
+                    if (volume_percentage_ != volume_percentage) {
+                        volume_percentage = volume_percentage_;
+                        duty_cycle = volume_percentage / 2;
+                        soft_pwm_controller_.configure(sound.frequency, duty_cycle);
+                    }
+                }
+            }
+        }
+
+        soft_pwm_controller_.enable(false);
+    });
+
+    if(playing_protocol == PlayingProtocol::blocking) {
+        work_thread.join();
+    }
+
+    return true;
 }
 
 void PiezoController::setVolume(uint8_t volume_percentage) {
+    if(volume_percentage > 100) volume_percentage = 100;
+
     volume_percentage_ = volume_percentage;
 }
 
-void PiezoController::flushQueue() {
-
+bool PiezoController::isPlaying() const {
+    return is_playing_;
 }
